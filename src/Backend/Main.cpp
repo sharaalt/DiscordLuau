2ae3c8b5fa42ec67ade5c5f546c1da3662289170
sharalt <sharafzadayousif@gmail.com>
2026-09-05 17:14:01 -0400

    Updating hierarchy

---
+++ src/Backend/Main.cpp
@@ -0,0 +1,24 @@
+﻿/*
+	@file Main.hpp
+	@author sharafzada
+	@date 2026-09-04
+	@brief The main entry point of the file.
+
+	Currently just used as a testing bed.
+*/
+
+#include <WebSocketManager.hpp>
+#include <GatewayClient.hpp>
+
+#include <iostream>
+
+int main() {
+	boost::asio::io_context ioContext;
+
+	WebSocketManager websocket(ioContext);
+	GatewayClient client(websocket, ioContext); // Pass onto luau for later use.
+
+	client.connect();
+
+	return 1;
+};
